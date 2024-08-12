19735558b849895e09936509573e78f1d579bb5f
karina <karynaromanouskaya@gmail.com>
2024-08-12 21:58:45 +0300

    Добавили тест

---
+++ src/test/java/DeliveryCardTest.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class DeliveryCardTest {
+
+    private String dateGenerate(int addDays, String dateFormat) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(dateFormat));
+    }
+    @Test
+    public void shouldSuccessDeliveryCard() {
+        open("http://localhost:9999");
+
+        $("[data-test-id='city'] input").setValue("Тамбов");
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id='date'] input").sendKeys(dateGenerate(24, "dd.MM.yyyy"));
+        $("[data-test-id='name'] input").setValue("Ромаовская Карина Владимировна");
+        $("[data-test-id='phone'] input").setValue("+79163305338");
+        $("[data-test-id=agreement]").click();
+
+        $$("button").find(Condition.exactText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(Condition.hidden, Duration.ofSeconds(100));
+        $(withText("Встреча успешно забронирована")).shouldBe(Condition.hidden, Duration.ofSeconds(100));
+
+        $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(100));
+        $("[data-test-id=notification]").shouldHave(Condition.text("Успешно!\n" + "Встреча успешно забронирована на " + dateGenerate(7, "dd.MM.yyyy"))).shouldBe(Condition.visible);
+    }
+}
