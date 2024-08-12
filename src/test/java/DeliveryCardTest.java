d85e5a84827bce419f8c9e5ad88ed08b54213652
karina <karynaromanouskaya@gmail.com>
2024-08-12 21:50:41 +0300

    добавили тест

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -8,28 +8,28 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
+
 public class DeliveryCardTest {
 
-    private String dateGenerate(int addDays) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    private String dateGenerate(int addDays, String dateFormat) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(dateFormat));
     }
-
     @Test
-    public void cardSuccessfullyBooked() {
+    public void shouldSuccessDeliveryCard() {
         open("http://localhost:9999");
 
-        $("[data-test-id='city'] input").setValue("Рязань");
+        $("[data-test-id='city'] input").setValue("Тамбов");
         $("[data-test-id=date] input").doubleClick();
-        $("[data-test-id='date'] input").sendKeys(dateGenerate(17));
-        $("[data-test-id='name'] input").setValue("Стариков Евгений Анатольевич");
-        $("[data-test-id='phone'] input").setValue("+79012345678");
+        $("[data-test-id='date'] input").sendKeys(dateGenerate(19, "dd.MM.yyyy"));
+        $("[data-test-id='name'] input").setValue("Полежаева Анна-Мария");
+        $("[data-test-id='phone'] input").setValue("+79202360184");
         $("[data-test-id=agreement]").click();
+
         $$("button").find(Condition.exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(Condition.hidden, Duration.ofSeconds(100));
         $(withText("Встреча успешно забронирована")).shouldBe(Condition.hidden, Duration.ofSeconds(100));
 
-
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(100));
-        $("[data-test-id=notification]").shouldHave(Condition.text("Успешно!\n" + "Встреча успешно забронирована на " + dateGenerate(5))).shouldBe(Condition.visible);
+        $("[data-test-id=notification]").shouldHave(Condition.text("Успешно!\n" + "Встреча успешно забронирована на " + dateGenerate(7, "dd.MM.yyyy"))).shouldBe(Condition.visible);
     }
 }
